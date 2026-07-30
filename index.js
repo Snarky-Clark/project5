a35ed2e91a332fd308628e0a36e3be1b0290f5b4
Erin Clark <erin.k.clark@proton.me>
2026-07-30 12:04:07 -0600

    Transfer project files

---
+++ index.js
@@ -0,0 +1,74 @@
+const navLinks = document.querySelectorAll("nav a");
+const sections = document.querySelectorAll("section");
+const themeButton = document.querySelector(".thememaster");
+const foodDrinkImage = document.querySelector("#food-drink-image");
+
+function hideAllSections() {
+    sections.forEach(function(section) {
+        section.classList.add("hidden");
+    });
+}
+
+function showSection(sectionId) {
+    const selectedSection = document.querySelector(sectionId);
+
+    if (selectedSection) {
+        selectedSection.classList.remove("hidden");
+    }
+}
+
+function turnOnTheme() {
+    let themeLink = document.querySelector("#theme-stylesheet");
+
+    if (!themeLink) {
+        themeLink = document.createElement("link");
+        themeLink.id = "theme-stylesheet";
+        themeLink.rel = "stylesheet";
+        themeLink.href = "theme.css";
+        document.head.appendChild(themeLink);
+    }
+
+    if (foodDrinkImage) {
+        foodDrinkImage.src = "bar.jpg";
+        foodDrinkImage.alt = "image of a bar";
+    }
+}
+
+function turnOffTheme() {
+    const themeLink = document.querySelector("#theme-stylesheet");
+
+    if (themeLink) {
+        themeLink.remove();
+    }
+    if (foodDrinkImage) {
+        foodDrinkImage.src = "kitchen2.jpg";
+        foodDrinkImage.alt = "image of kitchen";
+    }
+}
+
+hideAllSections();
+showSection("#Intro");
+
+navLinks.forEach(function(link) {
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        const sectionId = link.getAttribute("href");
+
+        hideAllSections();
+        showSection(sectionId);
+    });
+});
+
+if (themeButton) {
+    themeButton.addEventListener("click", function() {
+        console.log("Theme Master clicked");
+        turnOnTheme();
+    });
+
+    themeButton.addEventListener("dblclick", function() {
+        console.log("Theme Master double-clicked");
+        turnOffTheme();
+    });
+}
+
